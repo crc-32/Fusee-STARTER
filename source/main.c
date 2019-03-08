00e328d5c27065032957015e17bb3882773ac6df
crc-32 <thomas.c.england@gmail.com>
2019-03-08 22:24:55 +0000

    Code reformat + improvements

--- source/main.c
+++ source/main.c
@@ -7,99 +7,65 @@
 int down = 1;
 
 
+void openPayload(char* payloadPath) // Single open payload function to avoid repeating code
+{
+	FILE *f = fopen(payloadPath, "rb");
+	if (f == NULL) {
+		printf("Failed to open payload!\n");
+	} else {
+		fread(g_reboot_payload, 1, sizeof(g_reboot_payload), f);
+		fclose(f);
+		reboot_to_payload();
+	}
+}
+
 int main(int argc, char* argv[])
 {
-
 	consoleInit(NULL);
-	while (appletMainLoop()){
-	romfsInit();
-	  
+	while (appletMainLoop())
+	{
+		romfsInit();
 		hidScanInput();
 
 		u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
 		if (kDown & KEY_PLUS) break;
-
 		
-		if (down == 1) 
-		{
-			consoleClear();
-			printf("Welcome to Fusee-STARTER!\nSelect your payload and press A.\n--> 1. Launch hekate payload\n    2. Launch fusee payload\n    3. Lanch ArgonNX \n");
-		}
-		else if (down == 2) 
-		{
-			consoleClear();
-			printf("Welcome to Fusee-STARTER!\nSelect your payload and press A.\n    1. Launch hekate payload\n--> 2. Launch fusee payload\n    3. Lanch ArgonNX \n");
-		}
-		else if (down == 3) 
-		{
-			consoleClear();
-			printf("Welcome to Fusee-STARTER!\nSelect your payload and press A.\n    1. Launch hekate payload\n    2. Launch fusee payload\n--> 3. Lanch ArgonNX \n");
-		}
+		consoleClear();
+		printf("Welcome to Fusee-STARTER!\nSelect your payload and press A.\n");
+		printf("%s1. Launch hekate payload\n%s2. Launch fusee payload\n%s3. Lanch ArgonNX \n", 
+			(down==1) ? "--> " : "    ", // If condition in brackets is true, the left side of : is used, otherwise the right side of : is used
+			(down==2) ? "--> " : "    ",
+			(down==3) ? "--> " : "    ");
+		
 		if (kDown & KEY_A)
 		{
-			if (down == 1)
-			{
 			Result rc = splInitialize();
-			FILE *f = fopen("romfs:/payload/hekate.bin", "rb");
-			if (f == NULL) {
-			printf("Failed to open hekate!\n");
-			} else {
-			fread(g_reboot_payload, 1, sizeof(g_reboot_payload), f);
-			fclose(f);
-			reboot_to_payload();
+			if(R_FAILED(rc))
+				fatalSimple(rc);
+			
+			if (down == 1) // Shorthand if statements are better when we just want to do one thing (no need for curly brackets {})
+				openPayload("romfs:/payload/hekate.bin");
+			else if (down == 2)
+				openPayload("romfs:/payload/fusee.bin");
+			else if (down == 3)
+				openPayload("romfs:/payload/argon.bin");
 		}
-		}
-		else if (down == 2)
-			{
-		Result rc = splInitialize();
-		FILE *f = fopen("romfs:/payload/fusee.bin", "rb");
-		if (f == NULL) {
-			printf("Failed to open fusee!\n");
-		} else {
-			fread(g_reboot_payload, 1, sizeof(g_reboot_payload), f);
-			fclose(f);
-			reboot_to_payload();
-		}
-		}
-		else if (down == 3)
-			{
-		Result rc = splInitialize();
-		FILE *f = fopen("romfs:/payload/argon.bin", "rb");
-		if (f == NULL) {
-			printf("Failed to open argon!\n");
-		} else {
-			fread(g_reboot_payload, 1, sizeof(g_reboot_payload), f);
-			fclose(f);
-			reboot_to_payload();
-		}
-		}
-		}
-		if (down == 0)
-		{
-			down = 3;
-		}
+
+		if (down == 0) down = 3;
 		if (kDown & KEY_DUP)
 		{
-			consoleClear();
-			if(down < 4) {
+			if(down < 4)
 				down--;
-			}
 			else
-			{
 				down = 3;
-		        }
 		}
 		if (kDown & KEY_DDOWN)
 		{
-			consoleClear();
-			if(down < 3) {
+			if(down < 3)
 				down++;
-			}
 			else
-			{
 				down = 1;
-		        }
 		}
 		consoleUpdate(NULL);
 	}
